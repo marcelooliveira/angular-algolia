bb56d16b4035e7327c25b0ad01a527d34dc8b6e2
marcelooliveira <mclricardo@gmail.com>
2020-11-02 19:00:06 -0300

    pt1 deployment to Firebase

--- pt1-angular-algolia/src/app/app.component.ts
+++ pt1-angular-algolia/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { data } from '../data/data';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pt1-angular-algolia';
+  dogs = data;
 }
